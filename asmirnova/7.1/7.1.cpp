ed4d13b332e9eecca5943e25c9717fd7ac6c2659
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:17:35 +0500

    Lab7

---
+++ asmirnova/7.1/7.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <cmath>
+#include <iostream>
+#include <locale>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n, i, j;
+    double a[100];
+    double s = 0;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (i != 1)
+        {
+            bool pr = true;
+            for (int j = 2; j <= sqrt(i); j++)
+            {
+                if (i % j == 0)
+                    pr = false;
+            }
+            if (pr == true) s = s + a[i];
+        }
+    }
+    cout << s << endl;
+}
+
---
+++ asmirnova/7.2/7.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a[100];
+    int i, n, nmin, nmax, sum, max, min;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+        cin >> a[i];
+    nmax = 1;
+    nmin = 1;
+    for (i = 1; i <= n; i++)
+    {
+        if (a[i] < a[nmin]) nmin = i;
+        if (a[i] > a[nmax]) nmax = i;
+    }
+    sum = 0;
+    if (nmin < nmax)
+    {
+        for (i = nmin; i <= nmax; i++)
+            sum = sum + a[i];
+        cout << sum << endl;
+    }
+    else if (nmin > nmax)
+    {
+        for (i = nmax; i <= nmin; i++)
+            sum = sum + a[i];
+        cout << sum << endl;
+    }
+    system("pause");
+}
