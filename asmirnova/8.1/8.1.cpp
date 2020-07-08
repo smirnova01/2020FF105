ce7b4660e2b19c60824e7ab1e90bc55f9fca3b8e
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:17:49 +0500

    Lab8

---
+++ asmirnova/8.1/8.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a[100][100] = { 0 };
+    int n, i, k, j;
+    cin >> n;
+    k = 0;
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            if (i == j)
+            {
+                a[i][j] = k;
+                k++;
+            }
+    for (i = 1; i <= n; i++)
+    {
+        cout << endl;
+        for (j = 1; j <= n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+    }
+    system("pause");
+    return 0;
+}
---
+++ asmirnova/8.2/8.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a[100][100];
+    int n, i, j, r;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            a[i][j] = rand() % 90 + 10;
+    cout << endl;
+    cout << " Исходная матрица: " << endl;
+    for (i = 1; i <= n; i++)
+    {
+        cout << endl;
+        for (j = 1; j <= n; j++)
+            cout << a[i][j] << " ";
+    }
+    cout << endl;
+    cout << " Получившаяся матрица: " << endl;
+    for (i = 1; i <= n; i++)
+    {
+        r = a[n][i];
+        a[n][i] = a[i][n];
+        a[i][n] = r;
+    }
+    for (i = 1; i <= n; i++)
+    {
+        cout << endl;
+        for (j = 1; j <= n; j++)
+            cout << a[i][j] << " ";
+    }
+    system("pause");
+    return 0;
+}
