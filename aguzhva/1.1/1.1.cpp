44121dd99e3c5f56a201b51a7d30f48aad99bc90
NastyaGuzhva <66954580+NastyaGuzhva@users.noreply.github.com>
2020-07-08 19:26:22 +0500

    1.1

---
+++ aguzhva/1.1/1.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;      
+int main(char* args[])
+{
+	int x, y;
+	cin >> x >> y; //Считывание
+	cout << endl << "Resoult:" << pow(1 - tan(x), 1 / tan(x)) + cos(x - y); //pow(число, степень)
+	return 0;
+}
