2072229f5607864005f480738ee0b13f81ca7380
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:17:14 +0500

    Lab6

---
+++ asmirnova/6.1/6.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int NNND(int n)
+{
+    int d;
+    d = 3;
+    while (d <= n && n % d != 0)
+    {
+        d = d + 2;;
+    }
+    if (n % d == 0) cout << "НННД= " << d << endl;
+    else cout << "НННД = 1, больше нет." << endl;
+    return n;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cin >> n;
+    NNND(n);
+}
