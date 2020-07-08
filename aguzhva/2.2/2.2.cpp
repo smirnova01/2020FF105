6cf4bd4645e6938379f9e235067a147fc98d9cb9
NastyaGuzhva <66954580+NastyaGuzhva@users.noreply.github.com>
2020-07-08 19:29:21 +0500

    Lab2

---
+++ aguzhva/2.2/2.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    double a, b, c;
+    cout << "Введите числа  ";
+    cin >> a >> b >> c;
+    if (fabs(a - b) == fabs(a - c)) cout << "Точки b и с находятся на одинаковом расстоянии от точки а" << endl;
+    else  if (fabs(a - b) > fabs(a - c)) cout << "Точка с находится ближе к точке а" << endl;
+    else cout << "Точка b находится ближе к точке а" << endl;
+    system("PAUSE");
+}
---
+++ aguzhva/2.4/2.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    double x, F;
+    cout << "Введите x: ";
+    cin >> x;
+    if (x <= 2) F = x * x + 4 * x + 5;
+    else F = 1 / (x * x + 4 * x + 5);
+    cout << "F(x)= " << F << endl;
+    system("PAUSE");
+}
