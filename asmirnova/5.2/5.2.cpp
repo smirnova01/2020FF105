37012c83baa29e7f7fb9759114170360c0626789
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:16:46 +0500

    Lab5

---
+++ asmirnova/5.2/5.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int  N, k, min;
+    cout << "Введите N: ";
+    cin >> N;
+    k = 0;
+    min = 10;
+    do {
+        if (N % 10 == min)
+            k++;
+        else if (N % 10 <= min)
+        {
+            min = N % 10;
+            k = 1;
+        }
+        N = N / 10;
+    } while (N != 0);
+    cout << k << endl;
+    system("PAUSE");
+}
