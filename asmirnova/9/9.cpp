bff43850da1ec6dfd00d9675c5f114a84e603a1f
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:18:05 +0500

    Lab9

---
+++ asmirnova/9/9.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char string[1000];
+    cin.getline(string, 1000);
+    int n = strlen(string);
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (string[i] == ':')
+        {
+            string[i] = '.';
+        }
+    }
+    for (int i = n / 2; i < n + 1; i++)
+    {
+        if (string[i] == '!')
+        {
+            string[i] = '.';
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << string[i];
+    cout << endl;
+    return 0;
+}
