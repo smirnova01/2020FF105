9f7a3785d5ac4a582ded0db1df639abc971fc22e
NastyaGuzhva <66954580+NastyaGuzhva@users.noreply.github.com>
2020-07-08 19:32:37 +0500

    Lab6

---
+++ aguzhva/6.1/6.1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream> 
+using namespace std;
+
+int max2(int a[100], int n)
+{ 
+    int imx1, imx2;
+    if (a[1] > a[2])
+    {
+    imx1 = a[1];
+    imx2 = a[2];
+    }
+    else
+    {
+    imx1 = a[2];
+    imx2 = a[1];
+    }
+    for (int i = 3; i <= n; i++)
+        if (a[i] > imx1)
+        {
+            imx2 = imx1;
+            imx1 = a[i];
+        }
+        else if (a[i] > imx2)  
+            imx2 = a[i];
+    cout << "max2= " << imx2 << endl;
+    return n;
+}
+int main() {
+    int n;
+    int a[100], temp;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    max2(a,n);
+    system("pause");
+    return 0;
+}
---
+++ aguzhva/6.3/6.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int Revers(int n, int a) {
+    if (n == 0)
+        return a;
+    a *= 10;
+    a += n % 10;
+    n /= 10;
+    Revers(n, a);
+
+}
+int main() {
+    int num, revers_num;
+    cin >> num;
+    revers_num = Revers(num, 0);
+    cout << revers_num;
+    system("pause");
+}
