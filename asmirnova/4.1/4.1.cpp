42366a310b1ea30724f5814e51da4c891a8f0b57
AnnaSmirnova <annadmitrievna174@yandex.ru>
2020-07-08 20:14:44 +0500

    Lab4

---
+++ asmirnova/4.1/4.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    double  R;
+    int N = 0;
+    int x, y;
+    cout << "Введите R: ";
+    cin >> R;
+    int d = R;
+    for (x = -d; x <= d; x++)
+        for (y = -d; y <= d; y++)
+            if (sqrt(x * x + y * y) <= R)
+                N++;
+    cout << N << endl;
+    system("PAUSE");
+}
+
---
+++ asmirnova/4.2/4.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int k, n;
+    double    с;
+    cout << "Введите к ";
+    cin >> k;
+    cout << "Введите n(max)>0 ";
+    cin >> n;
+    с = sqrt(k * n);
+    while (n > 0)
+    {
+        n = n - 1;
+        с = sqrt(с + k * n);
+    }
+    cout << "ответ " << с << endl;
+    return 0;
+}
