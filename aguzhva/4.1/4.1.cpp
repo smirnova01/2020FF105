94c9f5be9e6b1364648ec8643cbf330b06b0ad77
NastyaGuzhva <66954580+NastyaGuzhva@users.noreply.github.com>
2020-07-08 19:31:55 +0500

    Lab4

---
+++ aguzhva/4.1/4.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x, g, k, i;
+	k = 0;
+	for (x = 0; x <= 15; x++) {
+		g = x * x + x + 17;
+		for (i = 2; i <= g - 1; i++)
+			if (g % i == 0) k++;
+		if (k == 0) cout << "При х= " << x << ", g= " << g << " - простое число" << endl;
+	}
+	system("pause");
+}
---
+++ aguzhva/4.2/4.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i;
+	double a, P;
+	cin >> a >> n;
+	P = a;
+	for (i = 1; i <= n; i++)
+		P = P * (a - i * n);
+	cout << P << endl;
+	system("pause");
+}
