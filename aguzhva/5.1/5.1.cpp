e6262514a3bdb4e58e279ca2848703e8a1f35089
NastyaGuzhva <66954580+NastyaGuzhva@users.noreply.github.com>
2020-07-08 19:32:15 +0500

    Lab5

---
+++ aguzhva/5.1/5.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    double a, b, c, dt;
+    int  i;
+    cout << "Введите день, месяц и год: ";
+    cin >> a >> b >> c;
+    dt = 0;
+    if (b == 1) dt = a;
+    for (i = 1; i <= (b - 1); i++) {
+        if (i == 1) dt = dt + 31;
+        if (i == 2)
+            if (c / 4 != 0) dt = dt + 28;
+            else dt = dt + 29;
+        if (i == 3) dt = dt + 31;
+        if (i == 4) dt = dt + 30;
+        if (i == 5) dt = dt + 31;
+        if (i == 6) dt = dt + 30;
+        if (i == 7) dt = dt + 31;
+        if (i == 8) dt = dt + 31;
+        if (i == 9) dt = dt + 30;
+        if (i == 10) dt = dt + 31;
+        if (i == 11) dt = dt + 30;
+        if (i == 12) dt = dt + 31;
+    }
+    cout << "Это " << dt + a << "-ый день в году" << endl;
+    system("PAUSE");
+}
---
+++ aguzhva/5.2/5.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    double n;
+    cout << "Введите номер дня в году n= " << endl;
+    cin >> n;
+    if ((n >= 1) && (n <= 31))  cout << n << " Января" << endl;
+    if ((n >= 32) && (n <= 59))  cout << n - 31 << " Февраля" << endl;
+    if ((n >= 60) && (n <= 90))  cout << n - 59 << " Марта" << endl;
+    if ((n >= 91) && (n <= 120))  cout << n - 90 << " Апреля" << endl;
+    if ((n >= 121) && (n <= 151))  cout << n - 120 << " Мая" << endl;
+    if ((n >= 152) && (n <= 181))  cout << n - 151 << " Июня" << endl;
+    if ((n >= 182) && (n <= 212))  cout << n - 181 << " Июля" << endl;
+    if ((n >= 213) && (n <= 243))  cout << n - 212 << " Августа" << endl;
+    if ((n >= 244) && (n <= 273))  cout << n - 243 << " Сентября" << endl;
+    if ((n >= 274) && (n <= 304))  cout << n - 273 << " Октября" << endl;
+    if ((n >= 305) && (n <= 334))  cout << n - 304 << " Ноября" << endl;
+    if ((n >= 334) && (n <= 365))  cout << n - 334 << " Декабря" << endl;
+    system("PAUSE");
+}
